017e92e340d1374c4a495685a3f1d8ba1011d72f
atexcode <athar.techs@gmail.com>
2024-08-20 16:49:40 +0500

    Feature Added: Delete Object Icon on Object Transformer

--- src/components/AtexBuilder.vue
+++ src/components/AtexBuilder.vue
@@ -47,6 +47,8 @@ const itemBackgroundColor = ref('#ffffff');
 const activeObject = ref(null);
 let canvas;
 
+var deleteIcon = "data:image/svg+xml,%3C%3Fxml version='1.0' encoding='utf-8'%3F%3E%3C!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'%3E%3Csvg version='1.1' id='Ebene_1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' width='595.275px' height='595.275px' viewBox='200 215 230 470' xml:space='preserve'%3E%3Ccircle style='fill:%23F44336;' cx='299.76' cy='439.067' r='218.516'/%3E%3Cg%3E%3Crect x='267.162' y='307.978' transform='matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)' style='fill:white;' width='65.545' height='262.18'/%3E%3Crect x='266.988' y='308.153' transform='matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)' style='fill:white;' width='65.544' height='262.179'/%3E%3C/g%3E%3C/svg%3E";
+
 // Initialize Fabric.js canvas on component mount
 onMounted(async () => {
   canvas = new fabric.Canvas('atexCanvas', {
@@ -54,6 +56,16 @@ onMounted(async () => {
     height: 600,
   });
 
+  fabric.Object.prototype.controls.deleteControl = new fabric.Control({
+    x: -0.5,
+    y: 0.5,
+    offsetY: 0,
+    offsetX: 0,
+    cursorStyle: 'pointer',
+    mouseUpHandler: deleteObjectBySelection,
+    render: renderIcon(deleteImg),
+    cornerSize: 16
+  });
 
   await nextTick();
 
@@ -95,6 +107,33 @@ onMounted(async () => {
   //Keydown Handlers
   window.addEventListener('keydown', handleKeyDown);
 });
+
+
+/**
+ * Tranformation Controlers
+ */
+var deleteImg = document.createElement('img');
+deleteImg.src = deleteIcon;
+
+const renderIcon = (icon) => {
+  return function renderIcon(ctx, left, top, styleOverride, fabricObject) {
+    console.log(styleOverride);
+    var size = this.cornerSize;
+    ctx.save();
+    ctx.translate(left, top);
+    ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
+    ctx.drawImage(icon, -size / 2, -size / 2, size, size);
+    ctx.restore();
+  }
+}
+
+const deleteObjectBySelection = (eventData, transform) => {
+  var target = transform.target;
+  var canvas = target.canvas;
+  canvas.remove(target);
+  canvas.requestRenderAll();
+}
+
 
 /**
  * Add Text on the canvas
